422a1b82b9b886862a20d3ca528389c4061db7be
PaulJSD <paul.atolagbe.ayobami@gmail.com>
2025-10-18 01:10:54 +0100

    fixed deployment issue

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -3,14 +3,28 @@ import { DataSource } from 'typeorm';
 
 config();
 
+const isProduction = !!process.env.DB_URL;
+
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST ?? 'localhost',
-  port: parseInt(process.env.DB_PORT ?? '5432'),
-  username: process.env.DB_USER ?? 'postgres',
-  password: process.env.DB_PASSWORD ?? 'changethepassword',
-  database: process.env.DB_DATABASE ?? 'postgres',
+  ...(isProduction
+    ? {
+        url: process.env.DATABASE_URL, // For production (cloud)
+      }
+    : {
+        host: process.env.DB_HOST ?? 'localhost',
+        port: parseInt(process.env.DB_PORT ?? '5432'),
+        username: process.env.DB_USER ?? 'postgres',
+        password: process.env.DB_PASSWORD ?? 'changethepassword',
+        database: process.env.DB_DATABASE ?? 'postgres',
+      }),
+
   synchronize: false,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/src/migrations/*{.ts,.js}'],
+  ssl: isProduction
+    ? {
+        rejectUnauthorized: false, // needed for providers like Neon.tech
+      }
+    : false,
 });
